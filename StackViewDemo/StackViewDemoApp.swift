0fb1b7d02819ddcf839d4f492c64ef2fca6562ea
NPSoftware68 <thanhtra.sqcb@icloud.com>
2023-02-28 09:12:58 +0700

    Initial Commit

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by Simon Ng on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
